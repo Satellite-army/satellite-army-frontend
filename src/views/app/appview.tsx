9fdcc68740d91645f47b219e6beb5c6aa10e1ce7
kvuote <kvuote@kvuote.com>
2021-08-16 11:20:45 +0200

    edited menu links

--- src/views/app/appview.tsx
+++ src/views/app/appview.tsx
@@ -13,13 +13,12 @@ export const AppView = (props: any)=>{
         {label:"dashboard", icon:"pi pi-home", index: 0, url: 'localhost:3000/app', command:()=>{ history.push("/app"); }},
         {label:"wallet", icon:"pi pi-wallet", index: 1, url: 'localhost:3000/wallet', command:()=>{ history.push("/app/wallet"); }},
         {label:"farming", icon:"pi pi-money-bill", index: 2, url: 'localhost:3000/farming', command:()=>{ history.push("/app/farming"); }},
-        {label:"analyitics", icon:"pi pi-chart-line", index: 3, url: 'http://primetek.com.tr', command:()=>{ history.push("/app"); }},
-        {label:"", icon:"", index: 99, url: 'http://primetek.com.tr', command:()=>{ history.push("/app"); }},
-        {label:"docs", icon:"pi pi-paperclip", index: 4, url: 'http://primetek.com.tr', command:()=>{ history.push("/app"); }},
-        {label:"twitter", icon:"pi pi-twitter", index: 5, url: 'http://primetek.com.tr', command:()=>{ history.push("/app"); }},
-        {label:"discord", icon:"pi pi-discord", index: 6, url: 'http://primetek.com.tr', command:()=>{ history.push("/app"); }},
-        {label:"telegram bot", icon:"pi pi-send", index: 7, url: 'http://primetek.com.tr', command:()=>{ history.push("/app"); }},
-        {label:"github", icon:"pi pi-github", index: 8, url: 'http://primetek.com.tr', command:()=>{ history.push("/app"); }}
+        {label:"", icon:"", index: 99, url: 'localhost:3000/app', command:()=>{ history.push("/app"); }},
+        {label:"docs", icon:"pi pi-paperclip", index: 4, url: 'http://satellite-army-book.s3-website-eu-west-1.amazonaws.com/', command:()=>{ history.push("/app"); }},
+        {label:"twitter", icon:"pi pi-twitter", index: 5, url: 'https://twitter.com/ArmySatellite', command:()=>{ history.push("/app"); }},
+        {label:"discord", icon:"pi pi-discord", index: 6, url: 'https://discord.com/invite/tdX3kqEe', command:()=>{ history.push("/app"); }},
+        {label:"telegram bot", icon:"pi pi-send", index: 7, url: 'https://telegram.me/satelliteArmyBot', command:()=>{ history.push("/app"); }},
+        {label:"github", icon:"pi pi-github", index: 8, url: 'https://github.com/Satellite-army/satellite-army-frontend', command:()=>{ history.push("/app"); }}
 
     ];
 
